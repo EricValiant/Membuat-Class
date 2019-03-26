c80912bae5d15a8932219c8a3f7cf5b9f2b31c81
EricValiant <ericvaliant.ev@gmail.com>
2019-03-26 13:48:34 +0700

    first commit

---
+++ Mahasiswa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Membuat_Class
+{
+    public class Mahasiswa
+    {
+        public string Nim   { get; set; }
+        public string Nama  { get; set; }
+        public float IPK    { get; set; }
+
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama : {0}", Nama);
+            Console.WriteLine("Nim : {0}", Nim);
+            Console.WriteLine("IPK : {0}", IPK);
+            Console.WriteLine("Telah melakukan registrasi");
+        }
+
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses Krs\n", Nama);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Membuat_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            saya.Nim = "12345";
+            saya.Nama = "Eric";
+            saya.IPK = 3.95f;
+
+            kamu.Nim = "123456";
+            kamu.Nama = "Rizal";
+            kamu.IPK = 3.70f;
+
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
